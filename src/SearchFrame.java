436d76f6a52044834ef770d139126a955733be8b
Maltob03 <46380730+Maltob03@users.noreply.github.com>
2023-01-14 13:58:46 +0100

    RESTORED CORRECTLY

--- src/SearchFrame.java
+++ src/SearchFrame.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.border.LineBorder;
 import javax.swing.border.MatteBorder;
 
 public class SearchFrame extends JFrame {
