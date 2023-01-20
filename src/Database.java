92909887be42b36235d92e1dcadc38aef9bb0a29
FilippoKJ <filippomariadalessandro@gmail.com>
2023-01-20 19:10:45 +0100

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class Database {
 
   //La keyword static serve invece a far si che un attributo di una classe non sia proprio dell'istanza ma comune all'oggetto.
-  static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/project_tarta";
+  static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/Esame";
   static final String USER = "postgres";
   static final String PASS = "admin";
   
